39674368e2bd7df1579978983be726f99de5f3a7
urion-sertor <orioldiscord1@gmail.com>
2026-03-01 16:28:30 +0100

    Roles added
    Added roles to diplo threads automatically without pinging

--- index.js
+++ index.js
@@ -8,17 +8,19 @@ const client = new Client({
 const TOKEN = process.env.TOKEN;
 const CATEGORY_ID = '1444360374903247159';
 const PORT = process.env.PORT || 8080; // Render asigns the port automatically
+const ROLE_SPECTATOR_ID = '1445014131064307875';
+const ROLE_UMPIRE_ID = '1444093999341244446';
 
 // HTTP Server
 http.createServer((req, res) => {
     res.writeHead(200);
-    res.end("Maester Online");
+    res.end("WARBOT is alive");
 }).listen(PORT, () => {
     console.log(`[SYSTEM] HTTP Server listening on port ${PORT}`);
 });
 
 client.once('ready', () => {
-    console.log(`[AUTH] Bot conectado como ${client.user.tag}`);
+    console.log(`[AUTH] Bot connected as ${client.user.tag}`);
 });
 
 client.on('threadCreate', async (thread) => {
@@ -27,6 +29,11 @@ client.on('threadCreate', async (thread) => {
         try {
             await thread.setRateLimitPerUser(3600);
             console.log(`[MOD] Slowmode set in ${thread.name}`);
+            const inviteMsg = await thread.send(`Including roles: <@&${ROLE_SPECTATOR_ID}> <@&${ROLE_UMPIRE_ID}>`); // Invita a los roles
+            console.log(`[INVITE] roles mentioned in ${thread.name}`);
+            setTimeout(() => { // Borra el mensaje 4 segundos después para eliminar los pings
+                inviteMsg.delete().catch(err => console.error("[ERROR] Ping couldn't get deleted:", err.message));
+            }, 4000);
         } catch (e) { console.error("[ERROR]", e.message); }
     }, 5000);
 });
